5f12fa9f71ef5f8d0451980f35cc9216a61dd6b3
ElhalwaguiAhmedMohamed <elhalwaguiahmedmohamed@gmail.com>
2022-03-30 22:58:30 +0200

    Add project files.

---
+++ WarehousesProject/Services/ImporterService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarehousesProject.Models;
+
+namespace WarehousesProject.Services
+{
+    public class ImporterService
+    {
+        public static ObservableCollection<Importer> getAll()
+        {
+            ObservableCollection<Importer> importers = new ObservableCollection<Importer>();
+            Model1 model = new Model1();
+            foreach (Importer importer in model.Importers)
+            {
+                importers.Add(importer);
+            }
+            return importers;
+        }
+
+        public static void addWarehouse(Importer _importer)
+        {
+            Model1 model = new Model1();
+            model.Importers.Add(_importer);
+            model.SaveChanges();
+        }
+
+        //public static void updateWarehouse(Importer _importer)
+        //{
+        //    Model1 model = new Model1();
+        //    Warhouse toBeUpdatedObj = model.Warhouses.Find(_importer.Id);
+        //    if (toBeUpdatedObj != null)
+        //    {
+        //        toBeUpdatedObj.Address = _warhouse.Address;
+        //        toBeUpdatedObj.Responsible = _warhouse.Responsible;
+        //        model.SaveChanges();
+        //    }
+        //}
+    }
+}
---
+++ WarehousesProject/ViewModels/importerViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarehousesProject.Services;
+using WarehousesProject.Models;
+using WarehousesProject.Commands;
+
+namespace WarehousesProject.ViewModels
+{
+    public class importerViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ObservableCollection<Importer> allImporters;
+
+        public ObservableCollection<Importer> AllImporters
+        {
+            get { return allImporters; }
+            set
+            {
+                allImporters = value;
+                onPropertyChanged("AllWareHouses");
+            }
+        }
+
+        private void onPropertyChanged(string _prop)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(_prop));
+        }
+
+        public importerViewModel()
+        {
+            AllImporters = ImporterService.getAll();
+        }
+
+
+
+
+    }
+}
